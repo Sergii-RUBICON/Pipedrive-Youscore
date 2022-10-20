30ead58430f66ee0d3826608213ae42ecebadf69
Serhienko Serhii <rubicon.sergii@gmail.com>
2022-10-20 23:45:15 +0300

    Тест массив парс 4

--- src/routers/checkSub.js
+++ src/routers/checkSub.js
@@ -4,8 +4,12 @@ const fields = require('../fields')
 
 
 async function checkSub(req, res) {
-    const payInfo = JSON.stringify(req.body)
-    console.log(`Інформація про платіж: ${JSON.parse(payInfo)}`)
+    const payInfo1st = JSON.stringify(req.body)
+    const payInfo2st = JSON.parse(payInfo1st)
+
+    console.log(`Інформація про платіж: ${payInfo1st}`)
+    console.log(`Інформація про платіж: ${payInfo2st.orderReference}`)
+    console.log(`Інформація про платіж: ${payInfo2st.value}`)
     res.end()
     //res.send({
     //    orderReference: req.body.orderReference,
