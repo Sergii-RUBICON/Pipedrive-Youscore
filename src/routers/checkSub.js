05f6c6a065be9171dbcf3472eefd5257334d63ed
Serhienko Serhii <rubicon.sergii@gmail.com>
2022-10-25 12:09:24 +0300

    Версія 12, добавиви необхідін логи

--- src/routers/checkSub.js
+++ src/routers/checkSub.js
@@ -7,13 +7,19 @@ async function checkSub(req, res) {
     //const payInf = JSON.parse(req.body)
     const str = Object.keys(req.body)[0] + '[]}'
     const payInf = JSON.parse(str)
-    console.log(`Інфо про портал: ${payInf.orderReference}`)
+    console.log(`Інфо про номер товару: ${payInf.orderReference}`)
+    console.log(`Інфо про назву товару: ${payInf.merchantAccount}`)
     console.log(`Інфо про портал: ${payInf}`)
-    res.send({
+    console.log(`Інфо про код відповіді: ${payInf.reasonCode}`)
+    console.log(`Інфо про відповідіь: ${payInf.reason}`)
+    console.log(`Інфо про час обробки: ${payInf.processingDate}`)
+
+    res.send(JSON.parse({
         orderReference: payInf.orderReference,
-        status: 'accept',
+        status: "accept",
         time: Date.now(),
-    })
+        signature: "",
+        }))
 }
 
 
