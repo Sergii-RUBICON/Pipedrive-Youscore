191aed9e1f4b7240c01e6793f1352f2aa8095b5e
Serhienko Serhii <rubicon.sergii@gmail.com>
2022-10-28 19:52:06 +0300

    Тест массив 15 (final)

--- src/routers/checkSub.js
+++ src/routers/checkSub.js
@@ -9,17 +9,17 @@ async function checkSub(req, res) {
     const payInf = JSON.parse(str)
     console.log(`Інфо про номер товару: ${payInf.orderReference}`)
     console.log(`Інфо про назву товару: ${payInf.merchantAccount}`)
-    console.log(`Інфо про портал: ${payInf}`)
+    console.log(`Інфо про портал: ${payInf.value}`)
     console.log(`Інфо про код відповіді: ${payInf.reasonCode}`)
     console.log(`Інфо про відповідіь: ${payInf.reason}`)
     console.log(`Інфо про час обробки: ${payInf.processingDate}`)
 
-    res.send(JSON.parse({
+    res.send({
         orderReference: payInf.orderReference,
         status: "accept",
         time: Date.now(),
         signature: "",
-        }))
+        })
 }
 
 
