229fe42667aeae1bc2e48b4abf7c25c5d1ccc670
Serhienko Serhii <rubicon.sergii@gmail.com>
2022-10-28 23:29:29 +0300

    Додав корректу відправку 4

--- src/routers/checkSub.js
+++ src/routers/checkSub.js
@@ -7,6 +7,7 @@ async function checkSub(req, res) {
     //const payInf = JSON.parse(req.body)
     const str = Object.keys(req.body)[0] + '[]}'
     const payInf = JSON.parse(str)
+    console.log(`Інфо: ${Object.values(req.body)}`)
     console.log(`Номер товару: ${payInf.orderReference}`)
     console.log(`Назву товару: ${payInf.merchantAccount}`)
     console.log(`Портал: ${payInf.value1}`)
@@ -15,10 +16,12 @@ async function checkSub(req, res) {
     console.log(`Статус відповідіь: ${payInf.reason}`)
     console.log(`Час обробки: ${payInf.processingDate}`)
 
+    let time = Date.now()
+
     res.json({
         "orderReference": payInf.orderReference,
         "status": "accept",
-        "time": Date.now(),
+        "time": time,
         "signature": "",
         })
 }
