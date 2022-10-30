645075e4f5760d1ec7556ef239c9e28667977a7c
Serhienko Serhii <rubicon.sergii@gmail.com>
2022-10-30 23:33:52 +0200

    Додав коректну відправку 5.2 /express.json()

--- src/routers/checkSub.js
+++ src/routers/checkSub.js
@@ -5,24 +5,24 @@ const fields = require('../fields')
 
 
 async function checkSub(req, res) {
-    console.log(req.rawBody)
-    const str = Object.keys(req.body)[0] + ''
-    const payInf = JSON.parse(str)
-    console.log(str)
+    console.log(req.body)
 
-    console.log(payInf.merchantAccount)
-    console.log(payInf.orderReference)
-    console.log(payInf.merchantSignature)
-    console.log(payInf.amount)
-    console.log(payInf.processingDate)
-    console.log(payInf.transactionStatus)
-    console.log(payInf.value)
+    console.log(req.body.merchantAccount)
+    console.log(req.body.orderReference)
+    console.log(req.body.merchantSignature)
+    console.log(req.body.amount)
+    console.log(req.body.processingDate)
+    console.log(req.body.transactionStatus)
+    console.log(req.body.value)
+
+
+    let time = Date.now()
 
     res.json({
-        orderReference: req.body.orderReference,
-        status: "accept",
-        time: Date.now(),
-        signature: payInf.merchantSignature,
+        "orderReference": req.body.orderReference,
+         "status": "accept",
+         "time": time,
+         "signature": "",
     })
 
 
