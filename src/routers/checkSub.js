7df6f84efdee3810df52ebd6b17ff81d2e83457c
Serhienko Serhii <rubicon.sergii@gmail.com>
2022-10-31 01:42:52 +0200

    Коррекктний парсинг 5.3 (test)

--- src/routers/checkSub.js
+++ src/routers/checkSub.js
@@ -6,9 +6,17 @@ const fields = require('../fields')
 
 async function checkSub(req, res) {
     console.log(req.body)
+    //unescape(JSON.parse('\\u00253A\\u00252F\\u00252...'));
+    const wayJson = req.body
+    const json = JSON.parse(wayJson)
+    const payInf = JSON.parse(json)
+    console.log(payInf)
+
+    /*
     const str = Object.keys(req.body)[0] + '[]}'
     const payInf = JSON.parse(str)
     console.log(payInf)
+
 
     console.log(req.body.merchantAccount)
     console.log(req.body.orderReference)
@@ -17,6 +25,7 @@ async function checkSub(req, res) {
     console.log(req.body.processingDate)
     console.log(req.body.transactionStatus)
     console.log(req.body.value)
+    */
 
 
     let time = Date.now()
