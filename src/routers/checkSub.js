3b88a19b5220abc06ba48c44a6f79bda88f1732f
Serhienko Serhii <rubicon.sergii@gmail.com>
2022-10-31 03:30:50 +0200

    Коррекктний парсинг 5.3 (test)

--- src/routers/checkSub.js
+++ src/routers/checkSub.js
@@ -8,22 +8,20 @@ const {json} = require("express");
 async function checkSub(req, res) {
     console.log(JSON.parse(JSON.stringify(req.body)))
     const payInf = JSON.parse(JSON.stringify(req.body))
-
-
+    const payObj = JSON.stringify(payInf)
 
     //const twice_json = req.body
     //const json = JSON.parse(twice_json)
     //const obj = JSON.parse(json)
     //console.log(obj.merchantAccount)
 
-
-    console.log(payInf[0].merchantAccount)
-    console.log(payInf[0].orderReference)
-    console.log(payInf[0].merchantSignature)
-    console.log(payInf[0].amount)
-    console.log(payInf[0].processingDate)
-    console.log(payInf[0].transactionStatus)
-    console.log(payInf[0].value)
+    console.log(payObj.merchantAccount)
+    console.log(payObj.orderReference)
+    console.log(payObj.merchantSignature)
+    console.log(payObj.amount)
+    console.log(payObj.processingDate)
+    console.log(payObj.transactionStatus)
+    console.log(payObj.value)
 
     /*
     const str = Object.keys(req.body)[0] + '[]}'
