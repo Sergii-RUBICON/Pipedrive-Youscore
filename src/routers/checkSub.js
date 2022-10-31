f6480713735ea18ab2e5d15a4853c0268f938e46
Serhienko Serhii <rubicon.sergii@gmail.com>
2022-10-31 11:46:28 +0200

    Коррекктний парсинг 5.3 (test)

--- src/routers/checkSub.js
+++ src/routers/checkSub.js
@@ -3,7 +3,7 @@
 async function checkSub(req, res) {
     console.log(JSON.parse(JSON.stringify(req.body)))
     const payInf = JSON.parse(JSON.stringify(req.body))
-    const payObj = JSON.parse(new payInf)
+    //const payObj = JSON.parse(payInf)
 
 
     //const twice_json = req.body
@@ -11,28 +11,28 @@ async function checkSub(req, res) {
     //const obj = JSON.parse(json)
     //console.log(obj.merchantAccount)
 
-    console.log(payObj.merchantAccount)
-    console.log(payObj.orderReference)
-    console.log(payObj.merchantSignature)
-    console.log(payObj.amount)
-    console.log(payObj.processingDate)
-    console.log(payObj.transactionStatus)
-    console.log(payObj.value)
-
-    /*
-    const str = Object.keys(req.body)[0] + '[]}'
-    const payInf = JSON.parse(str)
-    console.log(payInf)
+    //console.log(payObj.merchantAccount)
+    //console.log(payObj.orderReference)
+    //console.log(payObj.merchantSignature)
+    //console.log(payObj.amount)
+    //console.log(payObj.processingDate)
+    //console.log(payObj.transactionStatus)
+    //console.log(payObj.value)
 
 
-    console.log(req.body.merchantAccount)
-    console.log(req.body.orderReference)
-    console.log(req.body.merchantSignature)
-    console.log(req.body.amount)
-    console.log(req.body.processingDate)
-    console.log(req.body.transactionStatus)
-    console.log(req.body.value)
-    */
+    //const str = Object.keys(req.body)[0] + '[]}'
+    //const payInf = JSON.parse(str)
+    //console.log(payInf)
+
+
+    console.log(payInf ["merchantAccount"])
+    console.log(payInf[0].orderReference)
+    console.log(payInf[0].merchantSignature)
+    console.log(payInf[0].amount)
+    console.log(payInf[0].processingDate)
+    console.log(payInf[0].transactionStatus)
+    console.log(payInf[2].value)
+
 
     //res.json({
     //    "orderReference": req.body.orderReference,
@@ -41,8 +41,6 @@ async function checkSub(req, res) {
     //     "signature": "",
     //})
     res.end()
-
-
 }
 
 
