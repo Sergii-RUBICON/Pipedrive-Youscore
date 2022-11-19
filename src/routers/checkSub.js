ab506de22d2a759f1ea79d208d284d1c77f2585a
Serhienko Serhii <rubicon.sergii@gmail.com>
2022-11-19 21:54:16 +0200

    256

--- src/routers/checkSub.js
+++ src/routers/checkSub.js
@@ -1,13 +1,8 @@
 
 
 async function checkSub(req, res) {
-    console.log(req)
-    //let strObj = JSON.parse(JSON.stringify(req.body))
-    //console.log(strObj)
-    //let obj = strObj.toString().replace(/"/g)
-    //const obj2 = JSON.parse(JSON.stringify(obj))
-    //console.log(obj2)
 
+    console.log(req)
     res.end()
 }
 
--- src/routers/main.js
+++ src/routers/main.js
@@ -16,7 +16,7 @@ router.post('/createFields', createFields)
 router.post('/createWebhook', createWebhook)
 router.post('/', connectRouter)
 router.post('/checkWebhook', bodyParser.json(), checkWebhook)
-router.post('/checkSupPay', bodyParser.urlencoded({ extended: false}), bodyParser.json(), checkSub)
+router.post('/checkSupPay', bodyParser.urlencoded({ extended: false}), bodyParser.json(), await checkSub)
 
 module.exports = router
 
