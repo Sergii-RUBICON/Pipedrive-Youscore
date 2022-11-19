397091de8394f1c2d0a30f26fd59f6203396b30f
Serhienko Serhii <rubicon.sergii@gmail.com>
2022-11-19 22:51:33 +0200

    Work with req on checkSub.js file

--- src/routers/checkSub.js
+++ src/routers/checkSub.js
@@ -6,6 +6,8 @@ async function checkSub(req, res) {
     let finalString = dummyString.replaceAll('"', '')
     console.log("original string: " + dummyString)
     console.log("final string: " + finalString)
+    const obj = JSON.parse(finalString)
+    console.log("FINAL OBJ" + obj)
 
     res.end()
 }
