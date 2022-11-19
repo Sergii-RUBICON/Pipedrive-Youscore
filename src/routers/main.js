be7668e0314362e7e3b82308a1029cacda57184a
Serhienko Serhii <rubicon.sergii@gmail.com>
2022-11-20 00:35:57 +0200

    TRY ALL VARIANTS ONE MORE TIME ver1.8

--- src/routers/main.js
+++ src/routers/main.js
@@ -19,7 +19,7 @@ router.post('/createFields', createFields)
 router.post('/createWebhook', createWebhook)
 router.post('/', connectRouter)
 router.post('/checkWebhook', bodyParser.json(), checkWebhook)
-router.post('/checkSupPay', express.urlencoded({extended:false}), checkSub)
+router.post('/checkSupPay', express.raw(), checkSub)
 
 module.exports = router
 
