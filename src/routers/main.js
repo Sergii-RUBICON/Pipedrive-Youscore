868209af6280b87b54b354e5dafe98e9a8dc88a8
Serhienko Serhii <rubicon.sergii@gmail.com>
2022-11-19 16:26:09 +0200

    244

--- src/routers/main.js
+++ src/routers/main.js
@@ -6,6 +6,7 @@ const connectRouter = require('./connect')
 const createWebhook = require('./createWebhook')
 const createFields = require('./createFields')
 const checkSub = require('./checkSub')
+const {application, json} = require("express");
 const router = express()
 
 //router.use(bodyParser.urlencoded({ extended: true }))
@@ -16,6 +17,6 @@ router.post('/createFields', createFields)
 router.post('/createWebhook', createWebhook)
 router.post('/', connectRouter)
 router.post('/checkWebhook', bodyParser.json(), checkWebhook)
-router.post('/checkSupPay', bodyParser.json(), checkSub)
+router.post('/checkSupPay', bodyParser.urlencoded({ extended: false , type: application/json}), checkSub)
 
 module.exports = router
