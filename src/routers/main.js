ce28baa1ea9988d7441c967a6ae69e1c555a20bb
Serhienko Serhii <rubicon.sergii@gmail.com>
2022-11-19 20:44:58 +0200

    254

--- src/routers/main.js
+++ src/routers/main.js
@@ -16,6 +16,6 @@ router.post('/createFields', createFields)
 router.post('/createWebhook', createWebhook)
 router.post('/', connectRouter)
 router.post('/checkWebhook', bodyParser.json(), checkWebhook)
-router.post('/checkSupPay', bodyParser.urlencoded({ extended: false, inflate: false}), bodyParser.json(), checkSub)
+router.post('/checkSupPay', bodyParser.urlencoded({ extended: true }), bodyParser.json(), checkSub)
 
 module.exports = router
