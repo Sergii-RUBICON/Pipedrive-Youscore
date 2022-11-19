aecd4b5fc0d85218128d3c523a61479d0ebac387
Serhienko Serhii <rubicon.sergii@gmail.com>
2022-11-19 23:10:17 +0200

    Add log a req.body keys and values

--- src/routers/checkSub.js
+++ src/routers/checkSub.js
@@ -2,12 +2,18 @@
 
 async function checkSub(req, res) {
 
-    let dummyString = JSON.stringify(req.body)
-    let finalString = dummyString.replaceAll('"', '')
-    console.log("original string: " + dummyString)
-    console.log("final string: " + finalString)
-    const obj = JSON.parse(finalString)
-    console.log("FINAL OBJ" + obj)
+    const objKeys = Object.keys(req.body)
+    const objValues = Object.values(req.body)
+
+    console.log(objKeys)
+    console.log(objValues)
+
+    //let dummyString = JSON.stringify(req.body)
+    //let finalString = dummyString.replaceAll('"', '')
+    //console.log("original string: " + dummyString)
+    //console.log("final string: " + finalString)
+    //const obj = JSON.parse(finalString)
+    //console.log("FINAL OBJ" + obj)
 
     res.end()
 }
