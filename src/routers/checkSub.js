167ece5ed72ec33b07f28e922329f8610cafaa9f
Serhienko Serhii <rubicon.sergii@gmail.com>
2022-11-19 18:20:24 +0200

    254

--- src/routers/checkSub.js
+++ src/routers/checkSub.js
@@ -2,10 +2,11 @@
 
 async function checkSub(req, res) {
     console.log(req)
-    const obj = JSON.parse(JSON.stringify(req.body))
+    const strObj = JSON.parse(JSON.stringify(req.body))
+    console.log(strObj)
+    const obj = strObj.replaceAll('"', '')
     console.log(obj)
-    const obj2 = JSON.parse(JSON.stringify(obj))
-    console.log(obj2)
+
     res.end()
 }
 
