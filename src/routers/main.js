5268750536a22140c75b44d8d80d2ad1f635e05d
Serhienko Serhii <rubicon.sergii@gmail.com>
2022-11-21 15:41:31 +0200

    TRY ALL VARIANTS ONE MORE TIME FINAL

--- src/routers/main.js
+++ src/routers/main.js
@@ -17,6 +17,6 @@ router.post('/createFields', createFields)
 router.post('/createWebhook', createWebhook)
 router.post('/', connectRouter)
 router.post('/checkWebhook', bodyParser.json(), checkWebhook)
-router.post('/checkSupPay', bodyParser.urlencoded({ extended: false, type: "application/json" }), checkSub)
+router.post('/checkSupPay', bodyParser.json({ type: 'application/x-www-form-urlencoded' }), checkSub)
 
 module.exports = router
