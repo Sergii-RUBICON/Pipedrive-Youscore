5d305fb62f3a8b2edc0884ac7d52945232e8bb69
Serhienko Serhii <rubicon.sergii@gmail.com>
2022-11-21 15:33:52 +0200

    TRY ALL VARIANTS ONE MORE TIME FINAL

--- src/routers/main.js
+++ src/routers/main.js
@@ -9,7 +9,7 @@ const checkSub = require('./checkSub')
 const router = express()
 
 //router.use(bodyParser.json())
-router.use(bodyParser.urlencoded({ extended: false }))
+//router.use(bodyParser.urlencoded({ extended: false }))
 router.use(express.static(path.join(__dirname, 'public')))
 
 
@@ -17,6 +17,6 @@ router.post('/createFields', createFields)
 router.post('/createWebhook', createWebhook)
 router.post('/', connectRouter)
 router.post('/checkWebhook', bodyParser.json(), checkWebhook)
-router.post('/checkSupPay', checkSub)
+router.post('/checkSupPay', bodyParser.urlencoded({ extended: false, type: 'application/json' }), checkSub)
 
 module.exports = router
