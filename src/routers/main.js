19b525f5cf0d120faa1150f4949354dc5801c56c
Serhienko Serhii <rubicon.sergii@gmail.com>
2022-11-21 16:55:01 +0200

    TRY ALL VARIANTS ONE MORE TIME FINAL

--- src/routers/main.js
+++ src/routers/main.js
@@ -9,7 +9,7 @@ const checkSub = require('./checkSub')
 const router = express()
 
 router.use(express.json())
-router.use(express.urlencoded({ extended: true }))
+router.use(express.urlencoded({ extended: false }))
 router.use(express.static(path.join(__dirname, 'public')))
 
 
