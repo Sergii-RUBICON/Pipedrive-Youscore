11bdec3ee2ba3990c5d7453bbf9aed283b6c435a
Serhienko Serhii <rubicon.sergii@gmail.com>
2022-11-27 18:58:10 +0200

    Final payment parser v1.0

--- src/routers/checkSub.js
+++ src/routers/checkSub.js
@@ -2,6 +2,9 @@
 async function checkSub(req, res) {
 
     console.log(req)
+    const obj = JSON.parse(JSON.stringify(req.body))
+    const final = JSON.parse(obj)
+    console.log(final)
     //JSON.parse(rawStr.slice(1, rawStr.length - 1))
     //console.log(JSON.parse(req.rawBody))
     //console.log(req.body.merchantAccount)
