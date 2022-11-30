703eac67c38de0f6bfe583b6af301d3d3f851ee1
Serhienko Serhii <rubicon.sergii@gmail.com>
2022-11-30 13:45:08 +0200

    Response to WayForPay v1.0.1 (Adding console.logs)

--- src/routers/checkSub.js
+++ src/routers/checkSub.js
@@ -5,6 +5,14 @@ async function checkSub(req, res) {
     console.log(req.rawBody)
     console.log(obj)
 
+    console.log(obj.merchantAccount)
+    console.log(obj.orderReference)
+    console.log(obj.authCode)
+    console.log(obj.products[0].name)
+    console.log(obj.products[0].price)
+    console.log(obj.clientFields[0].name)
+    console.log(obj.clientFields[0].value)
+
     res.json({
         orderReference: obj.orderReference,
         status: "accept",
