e2610a634ba2576abb755576ae8191c4176cf9e3
Serhienko Serhii <rubicon.sergii@gmail.com>
2022-11-30 14:04:58 +0200

    Response to WayForPay v1.0.2 (Repair router connect)

--- src/routers/main.js
+++ src/routers/main.js
@@ -11,13 +11,13 @@ const router = express()
 //router.use(express.urlencoded({ extended: true }))
 router.use(express.static(path.join(__dirname, 'public')))
 
-router.post('/createFields', createFields)
-router.post('/createWebhook', createWebhook)
-router.post('/', connectRouter)
-router.post('/checkWebhook', bodyParser.json(), checkWebhook)
+router.post('/createFields', express.urlencoded({ extended: true }), createFields)
+router.post('/createWebhook', express.urlencoded({ extended: true }), createWebhook)
+router.post('/', express.urlencoded({ extended: true }), connectRouter)
+router.post('/checkWebhook', express.urlencoded({ extended: true }), bodyParser.json(), checkWebhook)
 router.post('/checkSupPay', express.urlencoded({ extended: true,
     verify(req, res, buf,) {
-    req.rawBody = buf.toString();
+    req.rawBody = buf.toString()
 }}), checkSub)
 
 module.exports = router
