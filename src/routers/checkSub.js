54b4d8186a61146d04ec8cd7f66398a83ec983e4
Serhienko Serhii <rubicon.sergii@gmail.com>
2022-12-03 14:34:46 +0200

    Response to WayForPay v1.0 (Start adding crypto hash HMAC_MD5)

--- src/routers/checkSub.js
+++ src/routers/checkSub.js
@@ -1,3 +1,4 @@
+const createHmac = require('crypto').createHmac
 
 async function checkSub(req, res) {
 
@@ -14,14 +15,25 @@ async function checkSub(req, res) {
     console.log(`Назва кастом поля: ${obj.clientFields[0].name}`)
     console.log(`Портал: ${obj.clientFields[0].value}`)
 
-    res.json({
+    let resObj = {
         orderReference: obj.orderReference,
         status: "accept",
-        time: Date.now(),
-        signature: obj.merchantSignature,
-    })
+        time: Date.now()
+    }
+    console.log('Res obj: ' + resObj)
+    resObj = resObj.toString()
+    console.log('Res str' + resObj)
+
     res.end()
 }
+
+
+//async function hmacmd5(string, secret = '59d84bc4cc1c61ea961c75688dd9105eb852128b') {
+//    const hash = createHmac('md5',secret)
+//        .update(string+";")
+//        .digest('hex');
+//    return hash
+//}
 
 
 module.exports = checkSub
