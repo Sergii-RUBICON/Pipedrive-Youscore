f55c89db161990896f5e78f0a4472ef3d608c50e
Serhienko Serhii <rubicon.sergii@gmail.com>
2022-12-03 15:04:17 +0200

    Response to WayForPay v1.3.0 (Start adding crypto hash HMAC_MD5)

--- src/routers/checkSub.js
+++ src/routers/checkSub.js
@@ -3,17 +3,17 @@ const createHmac = require('crypto').createHmac
 async function checkSub(req, res) {
 
     const obj = JSON.parse(req.rawBody)
-    console.log(req.rawBody)
-    console.log(obj)
+    //console.log(req.rawBody)
+    //console.log(obj)
 
-    console.log(`Мерчант: ${obj.merchantAccount}`)
-    console.log(`Номер заказу: ${obj.orderReference}`)
-    console.log(`Код авторизації: ${obj.authCode}`)
-    console.log(`Назва продукта: ${obj.products[0].name}`)
-    console.log(`Ціна продукдта: ${obj.products[0].price}`)
-    console.log(`Кількість продукта: ${obj.products[0].count}`)
-    console.log(`Назва кастом поля: ${obj.clientFields[0].name}`)
-    console.log(`Портал: ${obj.clientFields[0].value}`)
+    //console.log(`Мерчант: ${obj.merchantAccount}`)
+    //console.log(`Номер заказу: ${obj.orderReference}`)
+    //console.log(`Код авторизації: ${obj.authCode}`)
+    //console.log(`Назва продукта: ${obj.products[0].name}`)
+    //console.log(`Ціна продукдта: ${obj.products[0].price}`)
+    //console.log(`Кількість продукта: ${obj.products[0].count}`)
+    //console.log(`Назва кастом поля: ${obj.clientFields[0].name}`)
+    //console.log(`Портал: ${obj.clientFields[0].value}`)
 
     let resObj = {
         orderReference: obj.orderReference,
