2a245ce9c69f261580851c4b33f3aeae273c4cbe
Serhienko Serhii <rubicon.sergii@gmail.com>
2022-12-06 16:28:47 +0200

    Adding canculat date after pay v1.1.7

--- src/routers/checkSub.js
+++ src/routers/checkSub.js
@@ -2,19 +2,16 @@ const createHmac = require('crypto').createHmac
 const user = require('../db/user')
 
 async function checkSub(req, res) {
-
     const obj = JSON.parse(req.rawBody)
-    //console.log(req.rawBody)
-    //console.log(obj)
 
-    //console.log(`Мерчант: ${obj.merchantAccount}`)
-    //console.log(`Номер заказу: ${obj.orderReference}`)
-    //console.log(`Код авторизації: ${obj.authCode}`)
+    console.log(obj)
+    /*
     console.log(`Назва продукта: ${obj.products[0].name}`)
     console.log(`Ціна продукдта: ${obj.products[0].price}`)
     console.log(`Кількість продукта: ${obj.products[0].count}`)
     console.log(`Назва кастом поля: ${obj.clientFields[0].name}`)
     console.log(`Портал: ${obj.clientFields[0].value}`)
+    */
 
     let resObj = {
         orderReference: obj.orderReference,
@@ -22,6 +19,7 @@ async function checkSub(req, res) {
         time: Date.now(),
         signature: "",
     }
+
     console.log(resObj)
     const resHMC5 = await hmacmd5(resObj)
     console.log(resHMC5)
@@ -42,6 +40,7 @@ async function checkSub(req, res) {
 }
 
 
+
 async function hmacmd5(string, secret = '59d84bc4cc1c61ea961c75688dd9105eb852128b') {
     const hash = createHmac('md5',secret)
         .update(string+";")
@@ -49,6 +48,8 @@ async function hmacmd5(string, secret = '59d84bc4cc1c61ea961c75688dd9105eb852128
     return hash
 }
 
+
+//
 
 module.exports = checkSub
 
