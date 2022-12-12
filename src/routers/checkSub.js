34a8efe6e4647269105f95c8831487aeeb68b56f
Serhienko Serhii <rubicon.sergii@gmail.com>
2022-12-12 10:06:15 +0200

    Res to way for pay vol2 v1.2.3

--- src/routers/checkSub.js
+++ src/routers/checkSub.js
@@ -14,7 +14,7 @@ async function checkSub(req, res) {
             const calculateSub = await calculateDate(obj.products[0].name, userSub.subscription_end)
             await user.findOneAndUpdateStatusTime(obj.clientFields[0].value, calculateSub)
 
-            let resObj = obj.orderReference + ";" + "accept" + ";" + Date.now()
+            let resObj = obj.orderReference + ";" + "accept" + ";" + obj.processingDate
             console.log(resObj)
             const resHMC5 = await hmacmd5(resObj)
             console.log(resHMC5)
@@ -22,7 +22,7 @@ async function checkSub(req, res) {
             res.end(resHMC5)
         } else {
 
-            let resObj = `${obj.orderReference};"accept";${Date.now()}`
+            let resObj = obj.orderReference + ";" + "accept" + ";" + obj.processingDate
 
             console.log(resObj)
             const resHMC5 = await hmacmd5(resObj)
@@ -33,7 +33,7 @@ async function checkSub(req, res) {
     } else if (!userSub) {
         console.log('Value on custom field not correct, pls try again')
 
-        let resObj = `${obj.orderReference};"accept";${Date.now()}`
+        let resObj = obj.orderReference + ";" + "accept" + ";" + obj.processingDate
 
         console.log(resObj)
         const resHMC5 = await hmacmd5(resObj)
