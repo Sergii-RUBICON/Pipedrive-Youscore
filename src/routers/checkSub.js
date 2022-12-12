65bd25eef76e23f95335cdbe7aed3cee1f49d1c7
Serhienko Serhii <rubicon.sergii@gmail.com>
2022-12-12 17:03:55 +0200

    Res to way for pay vol2 v1.3.0

--- src/routers/checkSub.js
+++ src/routers/checkSub.js
@@ -25,7 +25,7 @@ async function checkSub(req, res) {
                 signature: md5Str,
             }
             console.log(resObj)
-            res.end(resObj)
+            res.json(resObj)
         } else {
             const str2 = obj.orderReference + ";" + "accept" + ";" + obj.createdDate
             console.log(str2)
@@ -39,7 +39,7 @@ async function checkSub(req, res) {
                 signature: md5Str,
             }
             console.log(resObj)
-            res.end(resObj)
+            res.json(resObj)
         }
     } else if (!userSub) {
         console.log('Value on custom field not correct, pls try again')
@@ -56,7 +56,7 @@ async function checkSub(req, res) {
             signature: md5Str,
         }
         console.log(resObj)
-        res.end(resObj)
+        res.json(resObj)
     }
 }
 
