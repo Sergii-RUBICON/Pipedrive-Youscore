7d73e42dd0f12cb9a333dbbb62b2c1379a1fb06c
Serhienko Serhii <rubicon.sergii@gmail.com>
2023-01-05 00:04:30 +0200

    Adding https on index.js (Fix bugs)

--- index.js
+++ index.js
@@ -169,11 +169,11 @@ app.use('/public', express.static(__dirname + '/public'))
 
 setupExpress(express)
 
-const cert = fs.readFileSync('cert/cert.pem', 'utf8')
-const key = fs.readFileSync('cert/privkey.pem', 'utf8')
-const credentials = { cert, key }
-const httpServer = http.createServer(app)
-const httpsServer = https.createServer(credentials, app)
+//const cert = fs.readFileSync('cert/cert.pem', 'utf8')
+//const key = fs.readFileSync('cert/privkey.pem', 'utf8')
+//const credentials = { cert, key }
+//const httpServer = http.createServer(app)
+//const httpsServer = https.createServer(credentials, app)
 
 //httpServer.listen(80)
 //httpsServer.listen(443, () => console.log(`🟢 App has started.\n🔗 Live URL: https://app.rubicon.tips`))
